e894a1c24096d2a3dd5d8ac69d20a3cf1b4f3ce3
Stefan Thomas <justmoon@members.fsf.org>
2011-08-22 23:47:40 +0100

    Add node hashbang as a triger for js2-mode.

--- moon.el
+++ moon.el
@@ -83,6 +83,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 ;; MuMaMo-Mode for rhtml files
 (add-to-list 'load-path "~/path/to/your/elisp/nxml-directory/util")
