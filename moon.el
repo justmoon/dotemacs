b9c4da9556bb37708b4201ba6efa9be664808a02
Stefan Thomas <justmoon@members.fsf.org>
2011-08-23 19:22:06 +0200

    Add customizations to moon.el.

--- moon.el
+++ moon.el
@@ -189,3 +189,24 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
 (require 'yasnippet)
 (yas/load-directory "~/.emacs.d/snippets")
+
+;; Color scheme
+(custom-set-faces
+ '(font-lock-builtin-face ((((class color) (min-colors 88) (background light)) (:foreground "#A52A2A"))))
+ '(font-lock-comment-face ((((class color) (min-colors 88) (background light)) (:foreground "#504984"))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background light)) (:foreground "#227A16"))))
+ '(font-lock-keyword-face ((((class color) (min-colors 88) (background light)) (:foreground "#BA1F1D" :weight semi-bold))))
+ '(font-lock-variable-name-face ((((class color) (min-colors 88) (background light)) (:foreground "#026396"))))
+ '(fringe ((((class color) (background light)) (:background "#F9FCFF"))))
+ '(js2-external-variable-face ((t (:foreground "#6A5ACD"))))
+ '(linum ((t (:inherit (shadow default)))))
+ '(mode-line ((((class color) (min-colors 88)) (:background "#6E97CA" :foreground "#FFFFFF" :box (:line-width -1 :style released-button)))))
+ '(mode-line-inactive ((default (:inherit mode-line)) (((class color) (min-colors 88) (background light)) (:background "#E6ECF3" :foreground "grey20" :box (:line-width -1 :color "grey75") :weight light))))
+ '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background light)) nil)))
+ '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background light)) (:background "#F7FBFF"))))
+ '(mumamo-border-face-in ((t (:inherit font-lock-preprocessor-face))))
+ '(mumamo-border-face-out ((t (:inherit font-lock-preprocessor-face))))
+ '(whitespace-space ((((class color) (background light)) (:foreground "#dddddd"))))
+ '(whitespace-tab ((((class color) (background light)) (:foreground "#dddddd"))))
+ '(whitespace-trailing ((t (:background "#ffffdd" :foreground "red" :weight bold)))))
+
