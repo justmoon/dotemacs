72e048c0e359e2a6653c26c398f5d6895a6655c3
Stefan Thomas <justmoon@members.fsf.org>
2012-11-12 16:24:04 -0800

    Add js2-mode, mooz fork.

--- moon.el
+++ moon.el
@@ -141,6 +141,11 @@
 (setq org-export-html-style
       "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://m.je/org/org-style.css\" />")
 
+;; JS2 mode - mooz fork
+(add-to-list 'load-path "~/.emacs.d/vendor/js2-mode")
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 ;; Color scheme
 (custom-set-faces
  '(font-lock-builtin-face ((((class color) (min-colors 88) (background light)) (:foreground "#A52A2A"))))
